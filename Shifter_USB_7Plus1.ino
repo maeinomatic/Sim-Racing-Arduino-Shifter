0dcef608dfc06f876e3ed5b03ea8c30e9005e7c7
Martin Potozki <61422800+maeinomatic@users.noreply.github.com>
2024-01-29 19:16:22 +0100

    Add files via upload

---
+++ Shifter_USB_7Plus1.ino
@@ -0,0 +1,31 @@
+#include <Joystick.h>
+
+// Define the Joystick object
+Joystick_ Joystick;
+
+// Define the pin numbers for buttons
+const int buttonPins[] = {2, 3, 4, 5, 6, 7, 8, 9};
+
+void setup() {
+  // Initialize the Joystick library
+  Joystick.begin();
+    Serial.begin(9600);
+
+  // Set button pins as inputs and enable internal pull-up resistors
+  for (int i = 0; i < 8; i++) {
+    pinMode(buttonPins[i], INPUT_PULLUP);
+  }
+}
+
+void loop() {
+  // Read button states and update Joystick buttons
+  for (int i = 0; i < 8; i++) {
+    Joystick.setButton(i + 0, !digitalRead(buttonPins[i])); // Button numbering starts from 1
+  }
+
+  // Send Joystick state
+  Joystick.sendState();
+
+  // Add a small delay to avoid sending data too quickly
+  delay(10);
+}
